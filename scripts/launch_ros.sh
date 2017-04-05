1b9e22501206298ff8dc46a3713537267c060b64
Andrea Del Prete <adelpret@laas.fr>
2017-04-05 18:20:05 +0200

    Add scripts to launch ros and sot on HRP2

---
+++ scripts/launch_ros.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=/opt/ros/indigo/lib/python2.7/dist-packages
+roscore
---
+++ scripts/run_sot.sh
@@ -0,0 +1,3 @@
+cd /home/adelpret/devel/hrp2-n14-system/_build-RELEASE/tools/direct-access/
+chrt --rr 40 ./run_sot_fast_da --input-file /home/adelpret/devel/openrobots/lib/libsot-hrp2-14-controller.so --dt=0.0025
+
