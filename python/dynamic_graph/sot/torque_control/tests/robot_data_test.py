36d3913582e0f13cf9c2f6e67ff8d23e0b741204
Olivier Stasse <ostasse@laas.fr>
2017-05-23 03:06:28 +0200

    [python] Add robot_data_test to gather all the data related to the robot (for testing).

---
+++ python/dynamic_graph/sot/torque_control/tests/robot_data_test.py
@@ -0,0 +1,37 @@
+
+testRobotPath="/opt/openrobots/share/hrp2_14_description/urdf/hrp2_14_reduced.urdf"
+controlDT=0.005
+maxCurrent=5
+
+mapJointNameToID={
+    'rhy': 0, 
+    'rhr': 1, 
+    'rhp': 2, 
+    'rk': 3,
+    'rap': 4,
+    'rar': 5,
+    'lhy': 6,
+    'lhr': 7,
+    'lhp': 8,
+    'lk': 9,
+    'lap': 10, 
+    'lar': 11, 
+    'ty': 12, 
+    'tp': 13, 
+    'hy': 14, 
+    'hp': 15, 
+    'rsp': 16, 
+    'rsr': 17, 
+    'rsy': 18, 
+    're': 19, 
+    'rwy': 20, 
+    'rwp': 21,
+    'rh': 22,
+    'lsp': 23,
+    'lsr': 24,
+    'lsy': 25,
+    'le': 26,
+    'lwy': 27,
+    'lwp': 28,
+    'lh': 29
+}
