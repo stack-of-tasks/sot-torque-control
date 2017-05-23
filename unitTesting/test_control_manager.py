c938ab4c3cbe2ecb1d4e059249240b08fb7c6214
Olivier Stasse <ostasse@laas.fr>
2017-05-23 03:07:25 +0200

    [python] Reflects previous changes to unitTesting.

--- unitTesting/test_control_manager.py
+++ unitTesting/test_control_manager.py
@@ -1,5 +1,6 @@
 from dynamic_graph.sot.core.matrix_util import matrixToTuple, vectorToTuple,rotate, matrixToRPY
 from dynamic_graph.sot.torque_control.control_manager import *
+from dynamic_graph.sot.torque_control.tests.robot_data_test import testRobotPath,controlDT,maxCurrent,mapJointNameToID
 from numpy import matrix, identity, zeros, eye, array, pi, ndarray, ones
 
 # Instanciate the free flyer
@@ -28,8 +29,11 @@ cm.signWindowsFilterSize.value = signWindowsFilterSize
 cm.tau.value = tau
 cm.tau_predicted.value = tau_predicted
 
+for key in mapJointNameToID:
+    cm.setNameToId(key,mapJointNameToID[key])
+
 # Init should be called before addCtrlMode because the size of state vector must be known.
-cm.init(0.005,"/opt/openrobots/share/hrp2_14_description/urdf/hrp2_14_reduced.urdf",5.0)
+cm.init(controlDT,testRobotPath,maxCurrent)
 
 ## Specify control mode ##
 # Add position mode
