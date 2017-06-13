6f9c902f8f6a7b0fd56c1c23cb918b8e8c6ed4cc
Andrea Del Prete <adelpret@laas.fr>
2017-06-13 17:51:19 +0200

    [python] Add new main function to build SoT graph for v3
    Conflicts:
    	hrp2-torque-control

--- python/dynamic_graph/sot/torque_control/main.py
+++ python/dynamic_graph/sot/torque_control/main.py
@@ -45,6 +45,54 @@ def main_pre_start(robot, delay=0.01, startSoT=True, go_half_sitting=True, urdfF
             go_to_position(robot.traj_gen, robot.halfSitting[6:], 10.0);
 
     return robot;
+    
+''' Main function to call before starting the graph. '''
+def main_pre_start_v3(robot, delay=0.01, startSoT=True, go_half_sitting=True, urdfFileName='/opt/openrobots/share/hrp2_14_description/urdf/hrp2_14.urdf'):
+    dt = robot.timeStep;
+    
+    # TMP: overwrite halfSitting configuration to use SoT joint order
+    robot.halfSitting = (
+         # Free flyer
+         0., 0., 0.648702, 0., 0. , 0.,
+         # Legs
+         0., 0., -0.453786, 0.872665, -0.418879, 0.,
+         0., 0., -0.453786, 0.872665, -0.418879, 0.,
+         # Chest and head
+         0., 0., 0., 0.,
+         # Arms
+         0.261799, -0.17453, 0., -0.523599, 0., 0., 0.1,
+         0.261799,  0.17453, 0., -0.523599, 0., 0., 0.1);
+         
+    robot.device.setControlInputType('position');
+    
+    robot.traj_gen        = create_trajectory_generator(robot.device, dt);
+    robot.com_traj_gen    = create_com_traj_gen(dt);
+    robot.rf_traj_gen     = SE3TrajectoryGenerator("tg_rf");
+    robot.lf_traj_gen     = SE3TrajectoryGenerator("tg_lf");
+    robot.rf_traj_gen.init(dt);
+    robot.lf_traj_gen.init(dt);
+    robot.estimator       = create_estimator(robot, dt, delay);
+
+    robot.ff_locator      = create_free_flyer_locator(robot, urdfFileName);
+
+    robot.pos_ctrl        = create_position_controller(robot, dt);
+    robot.torque_ctrl     = create_torque_controller(robot);
+    robot.inv_dyn         = create_balance_controller(robot, urdfFileName, dt);
+    robot.ctrl_manager    = create_ctrl_manager(robot, dt);
+    
+    robot.estimator.gyroscope.value = (0.0, 0.0, 0.0);
+#    estimator.accelerometer.value = (0.0, 0.0, 9.81);
+    if(startSoT):
+        print "Gonna start SoT";
+        sleep(1.0);
+        start_sot();
+
+        if(go_half_sitting):
+            print "Gonna go to half sitting";
+            sleep(1.0);
+            go_to_position(robot.traj_gen, robot.halfSitting[6:], 10.0);
+
+    return robot;
 
     
 ''' Main function to call after having started the graph. '''
