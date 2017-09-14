677ea4fa874ad158edb485b98553bb24ffde5e2e
Andrea Del Prete <adelpret@laas.fr>
2017-09-14 18:09:10 +0200

    [scripts] Change name of build directory of run_sot_fast_da

--- scripts/run_sot.sh
+++ scripts/run_sot.sh
@@ -1,3 +1,3 @@
-cd /home/adelpret/devel/hrp2-n14-system/_build-tmp/tools/direct-access/
+cd /home/adelpret/devel/hrp2-n14-system/_build-RELEASE/tools/direct-access/
 chrt --rr 40 ./run_sot_fast_da --input-file /opt/openrobots/lib/libsot-hrp2-14-controller.so --dt=0.0015
 
