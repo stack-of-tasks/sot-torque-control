d4bb7220af1991e5887969521e5c7ff39590d05a
andreadelprete <andre.delprete@gmail.com>
2017-09-19 16:17:20 +0200

    [python] Minor changes to identification scripts

--- python/dynamic_graph/sot/torque_control/identification/identify_motor_static.py
+++ python/dynamic_graph/sot/torque_control/identification/identify_motor_static.py
@@ -23,24 +23,53 @@ def identify_motor_static(enc, dq, ctrl, current, tau, JOINT_ID, JOINT_NAME, ZER
     maskConstNegAng=np.logical_and( maskConstAng ,maskNegVel ) 
     if SHOW_THRESHOLD_EFFECT :
         plt.figure()
-        plt.plot(enc); plt.ylabel('q')
+        plt.plot(enc, label='q')
         q_const=enc.copy()
         q_const[np.logical_not(maskConstAng)]=np.nan
-        plt.plot(q_const); plt.ylabel('q_const')
+        plt.plot(q_const, label='q_const')
+        plt.legend()
         
     # identify current sensor gain
     x = current[maskConstAng]
     y = ctrl[maskConstAng]/IN_OUT_GAIN
     maskPosErr = (y - x > 0.0)
     maskNegErr = (y - x < 0.0)
-    (Ksp,DZp)=solve1stOrderLeastSquare(x[maskPosErr], y[maskPosErr])
-    (Ksn,DZn)=solve1stOrderLeastSquare(x[maskNegErr], y[maskNegErr])
-    print "Current sensor gains = ", Ksp, Ksn;
-    print "Deadzones            = ", DZp, -DZn;
-    Ks = 0.5*(Ksp+Ksn);
-    DZ = 0.5*(DZp-DZn);
-    print "Avg current sensor gain = ", Ks;
-    print "Avg deadzone            = ", DZ;
+    print "Number of samples with constant angle:", x.shape[0]
+    print "Number of samples with constant angle and pos vel:", x[maskPosErr].shape[0]
+    print "Number of samples with constant angle and neg vel:", x[maskNegErr].shape[0]
+    if(x[maskPosErr].shape[0]<10):
+        (Ks,DZ)=solve1stOrderLeastSquare(x[maskNegErr], y[maskNegErr])
+    elif(x[maskNegErr].shape[0]<10):
+        (Ks,DZ)=solve1stOrderLeastSquare(x[maskPosErr], y[maskPosErr])
+    else:
+        (Ksn,DZn)=solve1stOrderLeastSquare(x[maskNegErr], y[maskNegErr])
+        (Ksp,DZp)=solve1stOrderLeastSquare(x[maskPosErr], y[maskPosErr])    
+        Ks = 0.5*(Ksp+Ksn);
+        Ks = 0.99*min([Ksp, Ksn])
+        DZ = 0.5*(DZp-DZn);    
+        print "Current sensor gains = ", Ksp, Ksn;
+        print "Deadzones            = ", DZp, -DZn;
+        
+        x_neg = x[maskNegErr]
+        y_neg = y[maskNegErr]
+        plt.figure()    
+        plt.plot(x_neg, y_neg,'.' ,lw=3,markersize=1,c='0.5');  
+        plt.plot([min(x_neg),max(x_neg)],[Ksn*min(x_neg)+DZn ,Ksn*max(x_neg)+DZn],'g:',lw=3)
+        plt.ylabel(r'$i(t)$'); plt.xlabel(r'$u(t)$')
+        plt.title('Negative current errors - Joint '+JOINT_NAME)
+        
+        x_pos = x[maskPosErr]
+        y_pos = y[maskPosErr]
+        plt.figure()    
+        plt.plot(x_pos, y_pos,'.' ,lw=3,markersize=1,c='0.5');  
+        plt.plot([min(x_pos),max(x_pos)],[Ksp*min(x_pos)+DZp ,Ksp*max(x_pos)+DZp],'g:',lw=3)
+        plt.ylabel(r'$i(t)$'); plt.xlabel(r'$u(t)$')
+        plt.title('Positive current errors - Joint '+JOINT_NAME)
+        plt.show()
+    
+    if(Ks<0.0):
+        print "ERROR: estimated Ks is negative! Setting it to 1"
+        Ks = 1.0;
     
     # plot dead zone effect ********************************************
     plt.figure()
@@ -54,8 +83,8 @@ def identify_motor_static(enc, dq, ctrl, current, tau, JOINT_ID, JOINT_NAME, ZER
     plt.ylabel(r'$i(t)$')
     plt.xlabel(r'$ctrl(t)-i(t)$')
     plt.plot(x,y,'.' ,lw=3,markersize=1,c='0.5');  
-    plt.plot(x[maskConstPosAng],y[maskConstPosAng],'rx',lw=3,markersize=1, label='pos vel'); 
-    plt.plot(x[maskConstNegAng],y[maskConstNegAng],'bx',lw=3,markersize=1, label='neg vel'); 
+    plt.plot(x[maskConstAng][maskPosErr],y[maskConstAng][maskPosErr],'rx',lw=3,markersize=1, label='pos err'); 
+    plt.plot(x[maskConstAng][maskNegErr],y[maskConstAng][maskNegErr],'bx',lw=3,markersize=1, label='neg err'); 
     plt.legend()
     
     plt.figure()
@@ -64,8 +93,8 @@ def identify_motor_static(enc, dq, ctrl, current, tau, JOINT_ID, JOINT_NAME, ZER
     plt.ylabel(r'$ctrl(t)$')
     plt.xlabel(r'$ctrl(t)-i(t)$')    
     plt.plot(x,y,'.' ,lw=3,markersize=1,c='0.5');  
-    plt.plot(x[maskConstPosAng],y[maskConstPosAng],'rx',lw=3,markersize=1, label='pos vel'); 
-    plt.plot(x[maskConstNegAng],y[maskConstNegAng],'bx',lw=3,markersize=1, label='neg vel'); 
+    plt.plot(x[maskConstAng][maskPosErr],y[maskConstAng][maskPosErr],'rx',lw=3,markersize=1, label='pos err'); 
+    plt.plot(x[maskConstAng][maskNegErr],y[maskConstAng][maskNegErr],'bx',lw=3,markersize=1, label='neg err'); 
     plt.legend()
     
     plt.figure()
@@ -100,13 +129,15 @@ def identify_motor_static(enc, dq, ctrl, current, tau, JOINT_ID, JOINT_NAME, ZER
     plt.ylabel(r'$i(t)$')
     plt.xlabel(r'$\tau(t)$')
     plt.title('Static experiment - Joint '+JOINT_NAME)
+
+    print "cur_sens_gain[%d] = %f" % (JOINT_ID, Ks);
+    print 'deadzone[%d]      = %f' % (JOINT_ID, DZ);    
+    print 'Kt_p[%d]          = %f' % (JOINT_ID,Ktp);
+    print 'Kt_n[%d]          = %f' % (JOINT_ID,Ktn);
+    print 'Kf_p[%d]          = %f' % (JOINT_ID,Kfp);
+    print 'Kf_n[%d]          = %f' % (JOINT_ID,Kfn);
     
-    print 'Kt_p[%d] = %f' % (JOINT_ID,Ktp);
-    print 'Kt_n[%d] = %f' % (JOINT_ID,Ktn);
-    print 'Kf_p[%d] = %f' % (JOINT_ID,Kfp);
-    print 'Kf_n[%d] = %f' % (JOINT_ID,Kfn);
-    
-    print 'Kt_m[%d] = %f' % (JOINT_ID,(Ktp+Ktn)/2.0);
-    print 'Kf_m[%d] = %f' % (JOINT_ID,(Kfp+Kfn)/2.0);
+    print 'Kt_m[%d]          = %f' % (JOINT_ID,(Ktp+Ktn)/2.0);
+    print 'Kf_m[%d]          = %f' % (JOINT_ID,(Kfp+Kfn)/2.0);
     
     return (Ktp, Ktn, Ks, DZ);
