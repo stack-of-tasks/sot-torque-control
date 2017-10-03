827a0f8c92ddb76edf8d56bb0e4e042836599847
andreadelprete <andre.delprete@gmail.com>
2017-10-03 14:17:21 +0200

    [madgwick] Check that norm of gradient is not zero before normalizing it

--- src/madgwickahrs.cpp
+++ src/madgwickahrs.cpp
@@ -115,6 +115,7 @@ namespace dynamicgraph
           s(3) = m_q3;
         }
         getProfiler().stop(PROFILE_MADGWICKAHRS_COMPUTATION);
+
         return s;
       }
 
@@ -183,17 +184,20 @@ namespace dynamicgraph
           s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * m_q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
           s2 = 4.0f * q0q0 * m_q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
           s3 = 4.0f * q1q1 * m_q3 - _2q1 * ax + 4.0f * q2q2 * m_q3 - _2q2 * ay;
-          recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
-          s0 *= recipNorm;
-          s1 *= recipNorm;
-          s2 *= recipNorm;
-          s3 *= recipNorm;
+          if(!((s0 == 0.0f) && (s1 == 0.0f) && (s2 == 0.0f) && (s3 == 0.0f)))
+          {
+            recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
+            s0 *= recipNorm;
+            s1 *= recipNorm;
+            s2 *= recipNorm;
+            s3 *= recipNorm;
 
-          // Apply feedback step
-          qDot1 -= m_beta * s0;
-          qDot2 -= m_beta * s1;
-          qDot3 -= m_beta * s2;
-          qDot4 -= m_beta * s3;
+            // Apply feedback step
+            qDot1 -= m_beta * s0;
+            qDot2 -= m_beta * s1;
+            qDot3 -= m_beta * s2;
+            qDot4 -= m_beta * s3;
+          }
         }
 
         // Integrate rate of change of quaternion to yield quaternion
