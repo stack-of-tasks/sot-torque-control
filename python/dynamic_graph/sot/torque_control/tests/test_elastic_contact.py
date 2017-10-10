434b263b7a0d089b69da6c652cfbc6b1a524d6f3
andreadelprete <andre.delprete@gmail.com>
2017-10-10 14:52:53 +0200

    [test-elastic-contact] Improve test

--- python/dynamic_graph/sot/torque_control/tests/test_elastic_contact.py
+++ python/dynamic_graph/sot/torque_control/tests/test_elastic_contact.py
@@ -9,12 +9,13 @@ except:
     print "Failed to load plot-utils"
 
 class Robot:
-    def __init__(self, m1, m2, kd, K, D, g, f1_stdev, x1=0, dx1=0, x2=0, dx2=0):
+    def __init__(self, m1, m2, B1, C1, K2, B2, g, f1_stdev, x1=0, dx1=0, x2=0, dx2=0):
         self.m1 = m1;
         self.m2 = m2;
-        self.kd = kd;
-        self.K = K;
-        self.D = D;
+        self.B1 = B1;
+        self.C1 = C1;
+        self.K2 = K2;
+        self.B2 = B2;
         self.g = g;
         self.f1_stdev = f1_stdev;
         self.x1 = x1;
@@ -22,47 +23,70 @@ class Robot:
         self.x2 = x2;
         self.dx2 = dx2;
         
-    def simulate(self, f1, dt):
-        # compute forces
-        self.f2 = -self.K*self.x2 - self.D*self.dx2;
-        self.f1 = f1 + normal(0, self.f1_stdev) - self.kd*self.dx1
-        # compute accelerations
-        self.ddx1 = self.g + (self.f1 )/self.m1;
-        self.ddx2 = self.g + (-self.f1 + self.f2)/self.m2;
-        # integrate
-        self.x1 += dt*self.dx1 + 0.5*dt*dt*self.ddx1;
-        self.x2 += dt*self.dx2 + 0.5*dt*dt*self.ddx2;
-        self.dx1 += dt*self.ddx1;
-        self.dx2 += dt*self.ddx2;
+    def simulate(self, f1, T, dt=0.0001):
+        N = int(T/dt);
+        assert(N>0)
+        f1_noise = normal(0, self.f1_stdev);
+        f1_sum = 0;
+        f2_sum = 0;
+        for i in range(N):        
+            # compute forces
+            self.f2 = -self.K2*self.x2 - self.B2*self.dx2;
+            self.f1 = f1 + f1_noise;
+            self.f1 -= self.B1*(self.dx1-self.dx2) + self.C1*np.sign(self.dx1-self.dx2)
+            f1_sum += self.f1;
+            f2_sum += self.f2;
+            # compute accelerations
+            self.ddx1 = self.g + (self.f1 )/self.m1;
+            self.ddx2 = self.g + (-self.f1 + self.f2)/self.m2;
+            # integrate
+            self.x1 += dt*self.dx1 + 0.5*dt*dt*self.ddx1;
+            self.x2 += dt*self.dx2 + 0.5*dt*dt*self.ddx2;
+            self.dx1 += dt*self.ddx1;
+            self.dx2 += dt*self.ddx2;
+        self.f1 = f1_sum/N;
+        self.f2 = f2_sum/N;
         
-        
-# m1*(ddx1-g) = f1
-# m2*(ddx2-g) = f2 - f1
-# f2 = -K*x2 - D*dx2
-m1 = 60.
-m2 = 1.
-kv = 3.*m1
-l = 0.09 # half distance between feet
-K = 2*2e5*l*l
-D = 0.2*sqrt(K)
-g = -9.81
-f1_stdev = 20.;
 
-f = 0.5;
-A = 0.05;
-kp = 30.0;
-kd = 0.0*sqrt(kp);
+# *** SYSTEM DESCRIPTION ***
+# q1 is the position of m1 w.r.t. the world
+# q2 is the position of m2 w.r.t. the world
+# we want to control q1
 
-x1_0 = 0.0;
-dx1_0 = 2*pi*f*A;
+# m1*(ddq1-g) = f1
+# m2*(ddq2-g) = f2 - f1
+# f2 = -K2*q2 - B2*dq2
+# f1 = f1d - B1*(dq1-dq2)
+
+m1 = 60.            # first mass
+m2 = 1.             # second mass
+B1 = .5*m1          # motor viscous friction
+C1 = 0.5*B1;        # motor Coulomb friction
+l = 0.09            # half distance between feet
+K2 = 10*2*2e5*l*l      # spring stiffness
+B2 = 0.01 * 2*sqrt(K2)   # damping
+g = -9.81           # gravity
+f1_stdev = 0.;      # motor standard deviation
+
+f = 0.5;            # desired trajectory frequency
+A = 0.05;           # desired trajectory amplitude
+kp = 10.0;          # controller proportional feedback gain
+kd = 2.0*sqrt(kp);  # controller derivative feedback gain
+kf = 0.0;           # force proportional gain
+ki = 5.0;           # force integral gain
+B1_ctrl = 0.9*B1;
+
 # at equilibrium we have: 
 # f1 = -m1*g
 # m1*g + m2*g = K*x2
 # x2 = (m1+m2)*g / K
-x2_0 = (m1+m2)*g/K
+x1_0 = 0.0;
+dx1_0 = 0*2*pi*f*A;
+x2_0 = (m1+m2)*g/K2
 dx2_0 = 0.0;
-dt = 0.001;
-T = 20;
+
+dt = 0.001;         # controller time step
+T = 15;             # total simulation time
 N = int(T/dt);
 PLOT_SPRING_DAMPER_RESPONSE = 0
 
@@ -81,7 +105,7 @@ if(PLOT_SPRING_DAMPER_RESPONSE):
     plt.legend()
     plt.show()
 
-robot = Robot(m1, m2, kv, K, D, g, f1_stdev, x1_0, dx1_0, x2_0, dx2_0);
+robot = Robot(m1, m2, B1, C1, K2, B2, g, f1_stdev, x1_0, dx1_0, x2_0, dx2_0);
 
 # integrate LDS
 two_pi_f = 2*pi*f;
@@ -90,22 +114,25 @@ x1_ref    = np.zeros(N);
 dx1_ref   = np.zeros(N);
 ddx1_ref  = np.zeros(N);
 
-f1   = np.zeros(N-1);
-f1_des = np.zeros(N-1);
-f2   = np.zeros(N-1);
+f1   = np.zeros(N);
+f1_ref = np.zeros(N);
+f1_des = np.zeros(N);
+f1_est = np.zeros(N);
+f2   = np.zeros(N);
 x1   = np.zeros(N);
 dx1  = np.zeros(N);
-ddx1 = np.zeros(N-1);
+ddx1 = np.zeros(N);
 x2   = np.zeros(N);
 dx2  = np.zeros(N);
-ddx2 = np.zeros(N-1);
-#e_Adt = expm(dt*G);
+ddx2 = np.zeros(N);
 
-for n in range(N-1):
+f1_err_int = 0
+f2[-1] =  -K2*x2_0 - B2*dx2_0;
+for n in range(N):
     # store data
-    x1[n] = robot.x1 + robot.x2;
+    x1[n] = robot.x1;
     x2[n] = robot.x2;
-    dx1[n] = robot.dx1 + robot.dx2;
+    dx1[n] = robot.dx1;
     dx2[n] = robot.dx2;
     
     # compute desired trajectory
@@ -116,20 +143,28 @@ for n in range(N-1):
     ddx1_des = ddx1_ref[n] + kd*(dx1_ref[n]-dx1[n]) + kp*(x1_ref[n]-x1[n])
 
     # compute actuator force
-    f1_des[n] = m1*(ddx1_des-g);
+    f1_ref[n] = m1*(ddx1_des-g)
+    f1_est[n] = f2[n-1] + m2*g
+    f1_err = f1_ref[n] - f1_est[n]
+    f1_err_int += ki*dt*f1_err
+    f1_des[n] = f1_ref[n] + kf*f1_err + f1_err_int + B1_ctrl*(dx1[n]-dx2[n])
 
-    # simulate    
+    # simulate
     robot.simulate(f1_des[n], dt);
 
     # store data
     f1[n] = robot.f1;
     f2[n] = robot.f2;
     ddx1[n] = robot.ddx1;
-    ddx2[n] = robot.ddx2;    
+    ddx2[n] = robot.ddx2;
+    
+print "Pos tracking error:  ", np.mean(np.abs(x1_ref-x1))
+print "Vel tracking error:  ", np.mean(np.abs(dx1_ref-dx1))
+print "Force tracking error:", np.mean(np.abs(f1_ref-f1))
 
 plt.figure()
 plt.plot(x1, label='x1');
-plt.plot(x1_ref, '-', label='x1 des', alpha=0.6)
+plt.plot(x1_ref, '-', label='x1 ref', alpha=0.6)
 plt.plot(x2, label='x2');
 plt.legend()
 
@@ -145,9 +180,10 @@ plt.legend()
 #plt.plot(ddx2, label='ddx2', alpha=0.5);
 #plt.legend()
 
-#plt.figure()
-#plt.plot(f1, label='f1', alpha=0.5);
-#plt.plot(f1_des, '--', label='f1 des');
+plt.figure()
+plt.plot(f1_ref, '--', label='f1 ref');
+plt.plot(f1_est, label='f1 est');
+plt.plot(f1, label='f1', alpha=0.5);
 #plt.plot(f2, label='f2');
-#plt.legend()
+plt.legend()
 plt.show()
