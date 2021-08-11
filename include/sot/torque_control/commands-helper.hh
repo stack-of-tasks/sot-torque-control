de1f7b943b12cb1ea00e4c22aba3c7498e6bea5c
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-08-11 21:48:49 +0200

    move doc/make CommandVoid 5-8 to dynamic-graph
    ref. https://github.com/stack-of-tasks/dynamic-graph/issues/36

--- include/sot/torque_control/commands-helper.hh
+++ include/sot/torque_control/commands-helper.hh
@@ -9,6 +9,7 @@
 #include <boost/function.hpp>
 
 /* --- COMMON INCLUDE -------------------------------------------------- */
+#include <dynamic-graph/config.hh>
 #include <dynamic-graph/command.h>
 #include <dynamic-graph/command-direct-setter.h>
 #include <dynamic-graph/command-direct-getter.h>
@@ -23,17 +24,39 @@ using ::dynamicgraph::command::docCommandVerbose;
 using ::dynamicgraph::command::docCommandVoid0;
 using ::dynamicgraph::command::docCommandVoid1;
 using ::dynamicgraph::command::docCommandVoid2;
+using ::dynamicgraph::command::docCommandVoid3;
+using ::dynamicgraph::command::docCommandVoid4;
 using ::dynamicgraph::command::docDirectGetter;
 using ::dynamicgraph::command::docDirectSetter;
 using ::dynamicgraph::command::makeCommandVerbose;
 using ::dynamicgraph::command::makeCommandVoid0;
 using ::dynamicgraph::command::makeCommandVoid1;
 using ::dynamicgraph::command::makeCommandVoid2;
+using ::dynamicgraph::command::makeCommandVoid3;
+using ::dynamicgraph::command::makeCommandVoid4;
 using ::dynamicgraph::command::makeDirectGetter;
 using ::dynamicgraph::command::makeDirectSetter;
 }  // namespace torquecontrol
 }  // namespace sot
 }  // namespace dynamicgraph
+
+#if DYNAMIC_GRAPH_VERSION_AT_LEAST(4, 4, 0)
+namespace dynamicgraph {
+namespace sot {
+namespace dg = dynamicgraph;
+namespace torquecontrol {
+using ::dynamicgraph::command::docCommandVoid5;
+using ::dynamicgraph::command::docCommandVoid6;
+using ::dynamicgraph::command::docCommandVoid7;
+using ::dynamicgraph::command::docCommandVoid8;
+using ::dynamicgraph::command::makeCommandVoid5;
+using ::dynamicgraph::command::makeCommandVoid6;
+using ::dynamicgraph::command::makeCommandVoid7;
+using ::dynamicgraph::command::makeCommandVoid8;
+}  // namespace torquecontrol
+}  // namespace sot
+}  // namespace dynamicgraph
+#else
 
 namespace dynamicgraph {
 namespace command {
@@ -229,5 +252,6 @@ inline std::string docCommandVoid7(const std::string& doc, const std::string& ty
 
 }  // namespace command
 }  // namespace dynamicgraph
+#endif
 
 #endif  // __sot_torquecontrol_commands_helper_H__
